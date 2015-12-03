ac578cc7c979a82f7b1db0a65161d7fab94a8671
Shay Avraham <shay@picsize.co.il>
2015-12-03 13:05:59 +0200

    update info

--- js/app/appData.js
+++ js/app/appData.js
@@ -1,5 +1,5 @@
 var appData = {
-
+    /*
     titleOne: 'בית חב״ד קטמון ירושלים ת"ו', //Up to 40 charceters
     titleTwo: 'כל הלב לכל אחד', //Up to 40 charceters
 
@@ -8,6 +8,18 @@ var appData = {
     beitchabad_phone: '02-5670760',
     beitchabad_owner_name: 'הרב תמיר קסטיאל',
     main_page_url: 'http://coinz-admin.co.il/apps/chabadkatamon/',
+    */
 
+    appid: '592DB-2890C',          //token for pushwhoosh
+    beit_chabad_id: '10018',
+
+    titleOne: 'בית חב״ד קטמון ירושלים ת״ו', //Up to 40 charceters
+    titleTwo: 'כל הלב לכל אחד', //Up to 40 charceters
+
+    beitchabad_name: 'בית חב״ד גונן קטמון ירושלים ת״ו',
+    beitchabad_address: 'רח׳ אמציה 2 ירושלים 93148',
+    beitchabad_phone: '02-5670760',
+    beitchabad_owner_name: 'הרב תמיר קסטיאל',
+    main_page_url: 'http://coinz-admin.co.il/apps/chabadkatamon/index.html'
 }
 
